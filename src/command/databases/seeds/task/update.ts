cfd85e1f34f4e4a963504f01ef694328b2e5d2d5
tungbhrelipa <tungbh@relipasoft.com>
2023-12-06 15:19:58 +0700

    add seeding task data

--- src/command/databases/seeds/task/update.ts
+++ src/command/databases/seeds/task/update.ts
@@ -2,6 +2,8 @@ import { INestApplicationContext, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PostModule } from 'src/modules/posts/post.module';
+import { PostService } from 'src/modules/posts/services/post.service';
 import { TASK_TYPE } from 'src/modules/tasks/constants/task.constant';
 import { TaskService } from 'src/modules/tasks/services/task.service';
 import { TaskModule } from 'src/modules/tasks/task.module';
@@ -9,6 +11,7 @@ import { TaskModule } from 'src/modules/tasks/task.module';
 @Module({
   imports: [
     TaskModule,
+    PostModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB_URL),
   ],
@@ -19,17 +22,43 @@ export const updateTask = async () => {
   const app: INestApplicationContext =
     await NestFactory.createApplicationContext(CommandModule);
   const taskService = app.get(TaskService);
+  const postService = app.get(PostService);
 
-  await taskService.updateAll(
-    { taskType: TASK_TYPE.JOIN_DISCORD },
-    {
-      taskInfo: {
-        title: 'MindForgeX',
-        link: 'https://discord.gg/ZzUdMn6t',
-        serverId: '1161246114511601674',
-      },
-    },
-  );
+  // await taskService.updateAll(
+  //   { taskType: TASK_TYPE.JOIN_DISCORD },
+  //   {
+  //     taskInfo: {
+  //       title: 'MindForgeX',
+  //       link: 'https://discord.gg/ZzUdMn6t',
+  //       serverId: '1161246114511601674',
+  //     },
+  //   },
+  // );
+
+  // const posts = await postService.getListPost({});
+  // const taskData = posts.map((_post) => ({
+  //   postId: _post._id,
+  //   name: 'Subscribe Twitch channel',
+  //   description:
+  //     'Subscribe my channel to get the newest information and rewards',
+  //   userAddress: [],
+  //   taskType: TASK_TYPE.SUBSCRIBE_TWITCH,
+  //   taskInfo: {
+  //     title: 'RelaxBeats',
+  //     link: 'https://www.twitch.tv/relaxbeats',
+  //     serverId: 'relaxbeats',
+  //   },
+  // }));
+  // const tasks = await taskService.createMultiTasks(taskData);
+  // await Promise.all(
+  //   tasks.map((_task) => {
+  //     // change postService.postModel to public
+  //     return postService.postModel.updateOne(
+  //       { _id: _task.postId },
+  //       { $push: { tasks: _task._id } },
+  //     );
+  //   }),
+  // );
 
   await app.close();
   process.exit(0);
